82725f2629e5389880c3019f06764d613721e4d9
Nene1724 <22520937@gm.uit.edu.vn>
2025-11-04 11:59:57 +0700

    fix: change AMI architecture to x86_64 to match instance type

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -15,12 +15,12 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*-arm64"]
+    values = ["al2023-ami-*-x86_64"]
   }
 
   filter {
     name   = "architecture"
-    values = ["arm64"]
+    values = ["x86_64"]
   }
 }
 
